147c7d6c468e9d497cac1fe0ead37b65955ba969
Kailash E <kailash@kumonofmansfield.com>
2023-02-27 23:52:19 -0500

    Imports cleaned up, "scatter3D" changed to "scatter", graphs saved to png

--- Visualizing_3D_graphs.jl
+++ Visualizing_3D_graphs.jl
@@ -1,24 +1,23 @@
-using Pkg
+using Plots
+using Random
+using CSV
+using DataFrames
+
+# Install packages
+# using Pkg
+# Pkg.add("Plots")
+# Pkg.add("CSV")
+# Pkg.add("DataFrames")
 
 Pkg.status()
-
-using Random
 
 X = rand(100)
 Y = rand(100)
 Z = rand(100)
 
-scatter3D(X,Y,Z)
+scatter(X,Y,Z)
 
-using Pkg
-Pkg.add("CSV")
-Pkg.add("DataFrames")
-
-using CSV
-using DataFrames
-
-
-data1 = CSV.read("/Users/sophiestrey/Desktop/data/ASTROPHYSICS/leftovers/GAMA_CZ5Unj.csv", DataFrame)
+data1 = CSV.read("path/to/data/file.csv", DataFrame)
 
 x = data1[!, "RA"]
 y = data1[!, "DEC"]
@@ -29,32 +28,28 @@ G02 = data1[((data1[!, "RA"] .< 38.8) .& (data1[!, "RA"].>30.2) .& (data1[!, "DE
 
 typeof(data1)
 
-Pkg.add("Plots")
-using Plots
 
 scatter(G02[!,"RA"], G02[!, "DEC"])
 
-scatter3D(G02[!, "Z_HELIO"], G02[!, "RA"], G02[!, "DEC"])
+scatter(G02[!, "Z_HELIO"], G02[!, "RA"], G02[!, "DEC"])
 
 G09 = data1[((data1[!, "RA"].<141.0) .& (data1[!, "RA"].>129.0) .& (data1[!, "DEC"].<3.0) .& (data1[!, "DEC"].>-2.0)),:]
 G12 = data1[((data1[!, "RA"].<186.0) .& (data1[!, "RA"].>174.0) .& (data1[!, "DEC"].<2.0) .& (data1[!, "DEC"].>-3.0)),:]
 G15 = data1[((data1[!, "RA"].<223.5) .& (data1[!, "RA"].>211.5) .& (data1[!, "DEC"].<3.0) .& (data1[!, "DEC"].>-2.0)),:]
 G23 = data1[((data1[!, "RA"].<351.9) .& (data1[!, "RA"].>338.1) .& (data1[!, "DEC"].<-30.0) .& (data1[!, "DEC"].> -35.0)),:]
 
-scatter3D(G02[!, "Z_HELIO"], G02[!, "RA"], G02[!, "DEC"])
-scatter3D(G09[!, "Z_HELIO"], G09[!, "RA"], G09[!, "DEC"])
-scatter3D(G12[!, "Z_HELIO"], G12[!, "RA"], G12[!, "DEC"])
-scatter3D(G15[!, "Z_HELIO"], G15[!, "RA"], G15[!, "DEC"])
-scatter3D(G23[!, "Z_HELIO"], G23[!, "RA"], G23[!, "DEC"])
+scatter(G02[!, "Z_HELIO"], G02[!, "RA"], G02[!, "DEC"])
+scatter(G09[!, "Z_HELIO"], G09[!, "RA"], G09[!, "DEC"])
+scatter(G12[!, "Z_HELIO"], G12[!, "RA"], G12[!, "DEC"])
+scatter(G15[!, "Z_HELIO"], G15[!, "RA"], G15[!, "DEC"])
+scatter(G23[!, "Z_HELIO"], G23[!, "RA"], G23[!, "DEC"])
 
-scatter3D(G02[!, "Z_HELIO"], G02[!, "RA"], G02[!, "DEC"])
+scatter(G02[!, "Z_HELIO"], G02[!, "RA"], G02[!, "DEC"])
 
-scatter3D(G09[!, "Z_HELIO"], G09[!, "RA"], G09[!, "DEC"])
+scatter(G09[!, "Z_HELIO"], G09[!, "RA"], G09[!, "DEC"])
 
-scatter3D(G12[!, "Z_HELIO"], G12[!, "RA"], G12[!, "DEC"])
+scatter(G12[!, "Z_HELIO"], G12[!, "RA"], G12[!, "DEC"])
 
-scatter3D(G15[!, "Z_HELIO"], G15[!, "RA"], G15[!, "DEC"])
+scatter(G15[!, "Z_HELIO"], G15[!, "RA"], G15[!, "DEC"])
 
-scatter3D(G23[!, "Z_HELIO"], G23[!, "RA"], G23[!, "DEC"])
-
-
+scatter(G23[!, "Z_HELIO"], G23[!, "RA"], G23[!, "DEC"])
--- lookback_graphs_age(c,z).jl
+++ lookback_graphs_age(c,z).jl
@@ -1,26 +1,25 @@
-import Pkg
-using Pkg
-Pkg.add("Cosmology")
-
 using Cosmology
+using DataFrames
+using CSV
+using Plots
+
+# Install packages
+# using Pkg
+# Pkg.add("Cosmology")
+# Pkg.add("CSV")
+# Pkg.add("Plots")
+# Pkg.add("DataFrames")
+
+Pkg.status()
 
 #define cosmological model. For this example I will use the Planck 2015 
 #cosmological parameters but this can be easily modified. 
-
 c = cosmology(h=0.7, OmegaM=0.3, OmegaR=0)
 
 age(c, 0)
-
 age(c, 1.42) #z = 1.42
 
-using Pkg
-Pkg.add("CSV")
-Pkg.add("DataFrames")
-
-using CSV
-using DataFrames
-
-data1 = CSV.read("/Users/sophiestrey/Desktop/data/ASTROPHYSICS/leftovers/GAMA_CZ5Unj.csv", DataFrame)
+data1 = CSV.read("path/to/data/file.csv", DataFrame)
 data1
 
 data1.age = age.(Ref(c), data1.Z_HELIO)
@@ -33,13 +32,7 @@ G12 = data1[((data1[!, "RA"].<186.0) .& (data1[!, "RA"].>174.0) .& (data1[!, "DE
 G15 = data1[((data1[!, "RA"].<223.5) .& (data1[!, "RA"].>211.5) .& (data1[!, "DEC"].<3.0) .& (data1[!, "DEC"].>-2.0)),:]
 G23 = data1[((data1[!, "RA"].<351.9) .& (data1[!, "RA"].>338.1) .& (data1[!, "DEC"].<-30.0) .& (data1[!, "DEC"].> -35.0)),:]
 
-Pkg.add("Plots")
-using Plots
+scatter(G02[!, "age"], G02[!, "RA"], G02[!, "DEC"])
 
-scatter3D(G02[!, "age"], G02[!, "RA"], G02[!, "DEC"])
-
-Pkg.status()
-
-scatter3D
-
-
+# This saves the generated graph as a .png file in the current folder.
+savefig("lookback-graph.png")
