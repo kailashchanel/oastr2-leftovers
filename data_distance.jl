b3849ab2b11660b98f1c047d0353b97ba8aced9d
AlexanderJCS <98898166+AlexanderJCS@users.noreply.github.com>
2023-03-05 15:24:58 -0500

    Optimize from taking several hours to several mins

--- data_distance.jl
+++ data_distance.jl
@@ -1,36 +1,67 @@
-# watched this video on dataframes
-# https://www.youtube.com/watch?v=Pt8Iz4Udg2E&ab_channel=TheJuliaProgrammingLanguage
-
 using Pkg
 Pkg.add(["CSV", "DataFrames", "Cosmology", "Unitful"])
 using CSV, DataFrames, Cosmology, Unitful
 
 
-function make_distance_df(data)
-    distance_df = DataFrame(index1=[], index2=[], distance=[])
+function write_distances(distance_data, filestream)
+    for row in distance_data
+        write(filestream, string(Int(row[1]), ",", Int(row[2]), ",", row[3], "\n"))
+    end
+end
 
-    for i in range(1, size(data)[1])
+
+"Calculate the distance from one point and every other point. Will write the output to filename"
+function calculate_distances(data, filename, startindex, endindex)
+    println("Start function with filename $filename startindex $startindex endindex $endindex")
+    distances = []
+
+    filestream = open(filename, "a")
+    write(filestream, "v1,v2,dist\n")  # set up csv header
+
+    for i in range(startindex, endindex)
         for j in range(i + 1, size(data)[1] - 1)
             distance = sqrt(
                 (data[i, "RA"] - data[j, "RA"])^2 + 
                 (data[i, "DEC"] - data[j, "DEC"])^2 +
                 (ustrip(data[i, "age"]) - ustrip(data[j, "age"]))^2
             )
-
-            push!(distance_df, [i, j, distance])
+            
+            push!(distances, [i, j, distance])            
         end
 
-        println(string(i / size(data)[1], "% complete"))
+        write_distances(distances, filestream)
+        distances = []
     end
 
-    return distance_df
+    write_distances(distances, filestream)
+    println(string("Done with range ", startindex, " to ", endindex))
+    close(filestream)
 end
 
 
+"Add age values to the dataset"
 function add_age!(data)
     c = cosmology(h=0.7, OmegaM=0.3, OmegaR=0)
 
     data.age = age.(Ref(c), data.Z_HELIO)
+end
+
+
+"Does the same thing as the function calculate_distnaces however it is multithreaded and writes to multiple different output files"
+function calculate_distances_multithreaded(data)
+    startindex = 1
+    jump = size(data)[1] / (Threads.nthreads() + 1)
+    endindex = jump
+
+    for i in 1:Threads.nthreads()
+        println("Spawning thread with startindex $startindex and endindex $endindex, i = $i")
+        @Threads.spawn calculate_distances(data, string("output", i, ".csv"), Int(ceil(startindex)), Int(ceil(endindex)))
+
+        sleep(0.1)
+
+        startindex += jump
+        endindex += jump
+    end
 end
 
 
@@ -42,16 +73,18 @@ function main()
     add_age!(data)
 
     println("Calculating data groups")
+    G02 = data[((data[!, "RA"] .< 38.8) .& (data[!, "RA"].>30.2) .& (data[!, "DEC"].<-3.72) .& (data[!, "DEC"].>-10.25)),:]
     G09 = data[((data[!, "RA"].<141.0) .& (data[!, "RA"].>129.0) .& (data[!, "DEC"].<3.0) .& (data[!, "DEC"].>-2.0)),:]
     G12 = data[((data[!, "RA"].<186.0) .& (data[!, "RA"].>174.0) .& (data[!, "DEC"].<2.0) .& (data[!, "DEC"].>-3.0)),:]
     G15 = data[((data[!, "RA"].<223.5) .& (data[!, "RA"].>211.5) .& (data[!, "DEC"].<3.0) .& (data[!, "DEC"].>-2.0)),:]
     G23 = data[((data[!, "RA"].<351.9) .& (data[!, "RA"].>338.1) .& (data[!, "DEC"].<-30.0) .& (data[!, "DEC"].> -35.0)),:]
 
     println("Calculating distance matrix")
-    distance_df = make_distance_df(G09)
-    println("Done")
 
-    println(distance_df)
+    calculate_distances_multithreaded(G02)
+    
+    println("Done")
 end
+
 
 main()
