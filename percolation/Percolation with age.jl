2f213f6dbc32a5367f3eaf25483c2cad10c5def6
Alex <alexandercastronovo@gmail.com>
2023-04-18 17:28:41 -0400

    Fix some weird parsing error

--- percolation/Percolation with age.jl
+++ percolation/Percolation with age.jl
@@ -1,5 +1,5 @@
 using CSV, DataFrames, Cosmology, JLD2
-​
+
 function spherical_to_cart(ra, dec, dist)
     # Convert spherical coordinates to cartesian coordinates
     # RA and Dec are in degrees
@@ -10,51 +10,51 @@ function spherical_to_cart(ra, dec, dist)
     z = dist * sin(deg2rad(dec))
     return x, y, z
 end
-​
+
 data = CSV.read("GAMA_CZ5Unj.csv", DataFrame)
-​
+
 println("Calculating radial distance")
 function add_dist!(data)
     c = cosmology(h=0.7, OmegaM=0.3, OmegaR=0)
-​
+
     data.dist = comoving_radial_dist.(Ref(c), data.Z_HELIO)
 end
 add_dist!(data)
-​
+
 println("Calculating cosmological age")
 function add_age!(data)
     c = cosmology(h=0.7, OmegaM=0.3, OmegaR=0)
-​
+
     data.age = age.(Ref(c), data.Z_HELIO)
 end
 add_age!(data)
-​
+
 println("Calculating data groups")
 # G02 = data[((data[!, "RA"] .< 38.8) .& (data[!, "RA"].>30.2) .& (data[!, "DEC"].<-3.72) .& (data[!, "DEC"].>-10.25)),:]
 # G09 = data[((data[!, "RA"].<141.0) .& (data[!, "RA"].>129.0) .& (data[!, "DEC"].<3.0) .& (data[!, "DEC"].>-2.0)),:]
 # G12 = data[((data[!, "RA"].<186.0) .& (data[!, "RA"].>174.0) .& (data[!, "DEC"].<2.0) .& (data[!, "DEC"].>-3.0)),:]
 G15 = data[((data[!, "RA"].<223.5) .& (data[!, "RA"].>211.5) .& (data[!, "DEC"].<3.0) .& (data[!, "DEC"].>-2.0)),:]
 # G23 = data[((data[!, "RA"].<351.9) .& (data[!, "RA"].>338.1) .& (data[!, "DEC"].<-30.0) .& (data[!, "DEC"].> -35.0)),:]
-​
+
 ra = G15[1:4:end, "RA"]
 dec = G15[1:4:end, "DEC"]
 distunit = G15[1:4:end, "dist"]
 ageunit = G15[1:4:end, "age"]
-​
+
 #remove units
 dist = Real.(distunit ./ oneunit.(distunit))
 cage = Real.(ageunit ./ oneunit.(ageunit))
 datalen = length(ra)
-​
+
 # Create a struct to store the distance data
 struct GalaxyDistance
     idx1::Int32
     idx2::Int32
     distance::Float32
 end
-​
+
 distance_list = GalaxyDistance[]
-​
+
 for i in 1:datalen-1
     println("i = $i")
     for j in i+1:datalen
@@ -63,11 +63,11 @@ for i in 1:datalen-1
         x1, y1, z1 = spherical_to_cart(ra[i], dec[i], dist[i])
         x2, y2, z2 = spherical_to_cart(ra[j], dec[j], dist[j])
         distance = sqrt((x1-x2)^2 + (y1-y2)^2 + (z1-z2)^2)
-​
+        
         push!(distance_list,GalaxyDistance(i,j,distance))
     end
 end
-​
+
 save_object("distance_list025.jld2", distance_list)
-​
+
 histogram([d.distance for d in distance_list], bins=100, xlabel="Distance (Mpc)", ylabel="Count")
--- percolation/percolation3.jl
+++ percolation/percolation3.jl
@@ -1,25 +1,28 @@
+import Pkg
+Pkg.add(["JLD2, Plots"])
+
 using JLD2, Plots
-​
+
 struct QuickFind
     id :: Vector{Int}
 end
-​
+
 function QuickFind(n::Int)
     id = collect(1:n)
     return QuickFind(id)
 end
-​
+
 function find(qf::QuickFind, p::Int)
     id = qf.id
     return id[p]
 end
-​
+
 function isconnected(qf::QuickFind, p::Int, q::Int)
     i = find(qf, p)
     j = find(qf, q)
     return isequal(i, j)
 end
-​
+
 function connect!(qf::QuickFind, p::Int, q::Int)
     id = qf.id
     pid = id[p]
@@ -32,7 +35,7 @@ function connect!(qf::QuickFind, p::Int, q::Int)
     end
     return
 end
-​
+
 """
     QuickUnion
 """
@@ -42,13 +45,13 @@ struct QuickUnion
     weighted :: Bool
     compress :: Bool
 end
-​
+
 function QuickUnion(n::Int, weighted=true, compress=true)
     id = collect(1:n)
     sz = zeros(Int, n)
     return QuickUnion(id, sz, weighted, compress)
 end
-​
+
 function find(qu::QuickUnion, p::Int)
     id = qu.id
     while p != id[p]
@@ -59,13 +62,13 @@ function find(qu::QuickUnion, p::Int)
     end
     return p
 end
-​
+
 function isconnected(qu::QuickUnion, p::Int, q::Int)
     i = find(qu, p)
     j = find(qu, q)
     return isequal(i, j)
 end
-​
+
 """
     connect(G, p, q)
 Connect p and q in G.
@@ -88,10 +91,9 @@ function connect!(qu::QuickUnion, p::Int, q::Int)
         id[i] = j
     end
 end
-​
+
 export QuickFind, QuickUnion, connect!, isconnected, find
-​
-​
+
 struct Percolation
     grid :: Matrix{Bool}
     li :: LinearIndices
@@ -102,7 +104,7 @@ struct Percolation
     node1 :: Int
     node2 :: Int
 end
-​
+
 function Percolation(ny, nx)
     grid = zeros(Bool, ny, nx)
     li = LinearIndices(grid)
@@ -110,44 +112,44 @@ function Percolation(ny, nx)
     wuf2 = QuickUnion(nx*ny + 1)
     return Percolation(grid, li, ny, nx, wuf1, wuf2, nx*ny + 1, nx*ny + 2)
 end
-​
+
 function connect!(p::Percolation, i, j)
     connect!(p.wuf1, i, j)
     connect!(p.wuf2, i, j)
 end
-​
+
 function isopen(p::Percolation, i, j)
     return p.grid[i, j]
 end
-​
+
 function number_of_open(p::Percolation)
     return sum(p.grid)
 end
-​
+
 function isfull(p::Percolation, i, j)
     !isopen(p, i, j) && return false
     return isconnected(p.wuf2, p.node1, p.li[i, j])
 end
-​
+
 function percolates(p::Percolation)
     return isconnected(p.wuf1, p.node1, p.node2)
 end
-​
+
 function open!(p::Percolation, i, j)
     isopen(p, i, j) && return
     p.grid[i, j] = true
-​
+
     i == 1       && connect!(p, p.node1, p.li[i, j])
     i == p.ny    && connect!(p.wuf1, p.node2, p.li[i, j])
-​
+
     i > 1        && isopen(p, i-1, j) && connect!(p, p.li[i, j], p.li[i-1, j])
     i < p.ny - 1 && isopen(p, i+1, j) && connect!(p, p.li[i, j], p.li[i+1, j])
     j > 1        && isopen(p, i, j-1) && connect!(p, p.li[i, j], p.li[i, j-1])
     j < p.nx - 1 && isopen(p, i, j+1) && connect!(p, p.li[i, j], p.li[i, j+1])
 end
-​
+
 using Statistics
-​
+
 struct Result{T}
     samples :: Vector{T}
     mean :: Float64
@@ -155,9 +157,9 @@ struct Result{T}
     confidence_lo :: Float64
     confidence_hi :: Float64
 end
-​
+
 const CONFIDENCE_95 = 1.96
-​
+
 function Result(samples)
     n = length(samples)
     m = mean(samples)
@@ -166,16 +168,16 @@ function Result(samples)
     ch = m + CONFIDENCE_95 * s / sqrt(n)
     return Result(samples, m, s, cl, ch)
 end
-​
+
 using Printf
-​
+
 function Base.show(io::IO, r::Result)
     @printf("% 25s =  %d\n", "samples", length(r.samples))
     @printf("% 25s =  %0.3f\n", "mean", r.mean)
     @printf("% 25s =  %0.3f\n", "stddev", r.stddev)
     @printf("% 25s = [%0.3f, %0.3f]\n", "95% confidence interval", r.confidence_lo, r.confidence_hi)
 end
-​
+
 function run_one(ny, nx)
     p = Percolation(ny, nx)
     while !percolates(p)
@@ -183,7 +185,7 @@ function run_one(ny, nx)
     end
     return p
 end
-​
+
 function run(n, trials)
     thresholds = zeros(trials)
     for i=1:trials
@@ -192,17 +194,17 @@ function run(n, trials)
     end
     return Result(thresholds)
 end
-​
+
 struct GalaxyDistance
     idx1::Int32
     idx2::Int32
     distance::Float32
 end
-​
+
 sorted_distance = load_object("sorted_distance_list.jld2")
-​
+
 side = Int(-.5 + sqrt(0.25+2*length(sorted_distance)))
-​
+
 function galaxy_run(sorted_distance,n)
     wuf1 = QuickUnion(n)
     clusters = Int32[]
@@ -214,15 +216,6 @@ function galaxy_run(sorted_distance,n)
     end
     return clusters
 end
-​
+
 clusters = galaxy_run(sorted_distance,side)
 Collapse
-
-
-
-
-
-
-
-
-
